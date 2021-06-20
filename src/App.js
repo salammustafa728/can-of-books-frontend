35db6b5c5b1b6c701142bb6bd73d675de21d9fde
baraaAlosaily <seelbaraa@gmail.com>
2021-06-20 23:02:56 +0300

    Baraa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './Login';
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import './Header.css';
 import LogoutButton from './Component/Logout';
 
 class Header extends React.Component {
   render() {
-    return(
+    return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-       
+        <Link to="/">- Home </Link>
+        <Link to="/profile"> - Profile -</Link>
+
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
-        <LogoutButton/>
+        <LogoutButton />
       </Navbar>
     );
   }
