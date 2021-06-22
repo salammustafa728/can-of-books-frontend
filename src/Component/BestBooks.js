2b975d9cddab8cf2af39d756571741e2d0decb90
salammustafa728 <salammustafa728@gmail.com>
2021-06-22 16:58:21 +0300

    edit

--- src/Component/BestBooks.js
+++ src/Component/BestBooks.js
@@ -1,51 +1,76 @@
 import React, { Component } from 'react'
 import { withAuth0 } from "@auth0/auth0-react";
 import axios from 'axios';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 
 export class BestBooks extends Component {
-constructor(props){
-    super(props);
-    this.state={
-        bookData:[],
-        booksStatus: false
-    }
-}
-componentDidMount = async () => {
-    await axios.get(`${process.env.REACT_APP_SERVER}/books?email=${this.props.auth0.user.email}`).then(response => {
-        this.setState({
-            bookData: response.data,
-            booksStatus: true
-        })
-    }).catch(
-        error=>{
-            alert(error.message)
+    constructor(props) {
+        super(props);
+        this.state = {
+            bookData: [],
+            booksStatus: false
         }
-    );
-}
+    }
+    componentDidMount = async () => {
+        await axios.get(`${process.env.REACT_APP_SERVER}/books?email=${this.props.auth0.user.email}`).then(response => {
+            this.setState({
+                bookData: response.data,
+                booksStatus: true
+            })
+        }).catch(
+            error => {
+                alert(error.message)
+            }
+        );
+    }
 
     render() {
         return (
             <div>
-                {this.state.booksStatus &&
-                <>
-                {this.state.bookData[0].Books.map(value=>{
-                    return (
-                        <>
-                             <h1>{value.name}</h1>
-                             <p>{value.description}</p>
-                             <p>{value.status}</p>
-                        </>
-                    )
-                })}
-                 {/* <img  /> */}
-                
-                </>
-                
+                {this.state.booksStatus > 0 &&
+                    <>
+                        {this.state.bookData[0].books.map(value => {
+                            return (
+                                <>
+                                    <Carousel>
+                                        <Carousel.Item>
+                                            <img
+                                                className="d-block w-100"
+                                                src="holder.js/800x400?text=First slide&bg=373940"
+                                                alt="First slide"
+                                            />
+                                            <Carousel.Caption>
+                                                <Card style={{ width: '18rem' }}>
+                                                    <Card.Img variant="top" src="holder.js/100px180" />
+                                                    <Card.Body>
+                                                        <Card.Title>{value.name}</Card.Title>
+                                                        <Card.Text>
+                                                        <p>{value.description}</p>
+                                                         <p>{value.status}</p>
+                                                        </Card.Text>
+                                                        <Card.Text>
+                                                         <p>{value.status}</p>
+                                                        </Card.Text>
+                                                        <Button variant="primary">Go somewhere</Button>
+                                                    </Card.Body>
+                                                </Card>
+                                                
+                                               
+                                            </Carousel.Caption>
+                                        </Carousel.Item>
+                                    </Carousel>
+                                </>
+                            )
+                        })}
+
+                    </>
+
                 }
-                
+
             </div>
         )
     }
--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import LogoutButton from './Component/Logout';
+import Login from './Component/LoginButton';
 
 class Header extends React.Component {
   render() {
@@ -12,9 +13,10 @@ class Header extends React.Component {
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <Link to="/">- Home </Link>
         <Link to="/profile"> - Profile -</Link>
-
+        
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
         <LogoutButton />
+       <Login/>
       </Navbar>
     );
   }
