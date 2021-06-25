9c1958d9d4e1984202ff54d0ffaa3a11096f6e90
salammustafa728 <salammustafa728@gmail.com>
2021-06-25 17:59:22 +0300

    edit

--- src/Component/UpdateFormModel.js
+++ src/Component/UpdateFormModel.js
@@ -5,21 +5,44 @@ export class UpdateBook extends Component {
     render() {
         return (
             <div>
-                <Form onSubmit={(evt) => this.props.updateMyBook(evt)}>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                {/* <Form onSubmit={(evt) => this.props.updateBook(evt)}>
+                    <Form.Group className="mb-3" controlId="name">
                         <Form.Label>Book Name</Form.Label>
-                        <Form.Control value={this.props.bookNameUpdate} onChange={(evt) => this.props.updateBookNameUpdate(evt.target.value)} />
+                        <Form.Control value={this.props.bookName}  />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Group className="mb-3" controlId="description">
                         <Form.Label>Discription</Form.Label>
-                        <Form.Control value={this.props.descriptionUpdate} as="textarea" row={5} onChange={(evt) => this.props.updatedescriptionUpdate(evt.target.value)} />
+                        <Form.Control value={this.props.description} as="textarea" row={5}/>
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Group className="mb-3" controlId="status">
                         <Form.Label>Status</Form.Label>
-                        <Form.Control value={this.props.statusUpdate}  onChange={(evt) => this.props.updatestatusUpdate(evt.target.value)} />
+                        <Form.Control value={this.props.status} />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Update Book
+                    </Button>
+                </Form> */}
+                <Form onSubmit={(e)=> this.props.updateBook(e)}>
+                    <Form.Group className="mb-3" controlId="name">
+                        <Form.Label>Edit New Book</Form.Label>
+                        <Form.Control 
+                         type="text" placeholder="Enter book name" />
+
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="desc">
+                       
+                        <Form.Control 
+                         type="text" placeholder="Enter book name" />
+
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="status">
+                       
+                        <Form.Control 
+                         type="text" placeholder="Enter book name" />
+
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Update
                     </Button>
                 </Form>
             </div>
